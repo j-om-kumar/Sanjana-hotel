0a4e300de1dfc982ca1445d6dd95db3e7b117907
Om Kumar <160997853+om-tericsoft1@users.noreply.github.com>
2025-01-17 01:25:41 +0530

    explanations

--- app/routes/checkin.py
+++ app/routes/checkin.py
@@ -5,15 +5,15 @@ from app.services.checkin import create_checkin,get_checkin,get_date_checkin
 router = APIRouter()
 
 
-@router.get("/check-in")
+@router.get("/check-in") # To get all the checkin's
 async def get_checkin_route():
     return await get_checkin()
 
-@router.post("/create-checkin")
+@router.post("/create-checkin") # To create a new check-in check CreateCheckIn for inputs
 async def create_checkin_route(payload: CreateCheckIn):
     return await create_checkin(payload)
 
-@router.post("/date/check-in")
+@router.post("/date/check-in") # to create a Check-in on a particular date check DateCheckIn for inputs
 async def get_date_checkin_route(payload:DateCheckIn):
     return await get_date_checkin(payload)
 
--- app/routes/db_asset.py
+++ app/routes/db_asset.py
@@ -5,19 +5,19 @@ from app.services.db_asset import create_asset,get_asset,add_asset,get_date_asse
 router = APIRouter()
 
 
-@router.get("/assets")
+@router.get("/assets") # To get all the assets from DB
 async def get_asset_route():
     return await get_asset()
 
-@router.get("/date/assets/{date}")
+@router.get("/date/assets/{date}") # to get assets on a particular data in space of date u must enter the date example '2024-01-15'
 async def get_date_asset_route(date):
     return await get_date_asset(date)
 
-@router.post("/create-asset")
+@router.post("/create-asset") # to create a new asset
 async def create_asset_route(payload: CreateAsset):
     return await create_asset(payload)
 
 
-@router.post("/add-asset")
+@router.post("/add-asset") # to add or remove values for pre-existing assets
 async def add_asset_route(payload: CreateAsset):
     return await add_asset(payload)
